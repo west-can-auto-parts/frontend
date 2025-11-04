1a595e4573384024c32664e0e7de05cf4530d756
west-can-auto-parts <info@westcanauto.com>
2025-11-04 15:48:10 +0530

    commit one line

--- src/app/(public)/about-us/_components/AboutWestCan.jsx
+++ src/app/(public)/about-us/_components/AboutWestCan.jsx
@@ -7,9 +7,9 @@ const AboutWestCan = () => {
         <p className='text-sm text-gray-500 text-justify'>
         West Can Auto Parts is the leading Canada auto parts distributor & wholesaler. It is a reputable and well-established auto parts supplier serving customers in the western region of Canada. With a rich history spanning several decades, the company has built a strong reputation for its commitment to providing high-quality Replacement Parts and excellent customer service.
         </p>
-        <p className="text-sm text-gray-500 mt-6 text-justify">
+        {/* <p className="text-sm text-gray-500 mt-6 text-justify">
         From humble beginnings, with more than 40 years of experience not only did we master the trade but also contributed to the industry’s advancement. In the early 1980’s we opened the doors to our first establishment in Northern British Columbia. Its success laid down the foundation for becoming industry leaders. West Can Auto Parts’ dynamic existence is attributed to its highly knowledgeable yet friendly staff.
-        </p>
+        </p> */}
     </div>
   )
 }
